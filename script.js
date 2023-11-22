54db6e8e0e6421cc6b644781a0f39b0232af3288
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 09:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
-};
-
+ const blob = new Blob([str]);
+	return blob.size;
+}
 
 // Do not change the code below
 const str = prompt("Enter some string.");
